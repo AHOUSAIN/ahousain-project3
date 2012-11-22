286ab40b61317fc56f0d82347319aefc71211102
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-22 08:39:28 +0300

    Gemfile updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AhousainProject3::Application.config.secret_token = '3464456b6af93ceb2ffe08280458f6dbdf7f63313198188e01d1939575dc3a27ea09b1a9b8f83a420c7eec1b3f16f82ae37eaeb60a56100b6ae78deddf1cfa70'
